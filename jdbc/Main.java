f7b62231aceda162638922048dd02e381fc92135
Divesh Patel <Diweshpatelsinha@gmail.com>
2026-04-06 07:32:34 +0530

    Main.java

---
+++ jdbc/Main.java
@@ -0,0 +1,32 @@
+package firstpackage;
+
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Student st = new Student();
+		//st.createDatabase();
+		//st.createTable();
+		
+//		st.createData();
+		
+		//st.createData2();
+		
+//		st.readData();
+		
+//		st.updateData(2, "Aditya", "Aditya@gmail.com");
+		
+//		st.deleteData(2);
+//		st.createData2();
+//		st.readData();
+		st.updateData2(2, "adi");
+		
+		
+		
+
+	}
+
+}
